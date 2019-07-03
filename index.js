311b88c66524b371d457d15e20afbeaa63bc0c8c
Jackson Kearl <jkearl@mit.edu>
2019-07-02 20:04:02 -0700

    Add dummy express app to keep heroku happy

--- index.js
+++ index.js
@@ -21,3 +21,9 @@ client.on('message', message => {
 });
 
 client.login('NTk1NDM3ODYwNzE1MzY0MzU4.XRrC9g.nokX-AJvuWTE8khPxdCdToultTI')
+
+
+// Dummy thing to bind to heroku port/trigger the app to re-launch
+require('express')()
+    .get('/', (req, res) => res.end('up!')) 
+    .listen(process.env.port || 3000)
