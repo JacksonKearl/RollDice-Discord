8bef8b3f9aace00c4503e763f6b9a4d131870027
Jackson Kearl <jkearl@mit.edu>
2019-07-02 19:16:53 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let Discord = require('discord.js')
+
+let client = new Discord.Client()
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('messageUpdate', (old, dlo) => {
+    if (dlo.content === 'ping') {
+        dlo.channel.send('gonp') 
+    } 
+})
+
+client.on('message', message => {
+    // If the message is "ping"
+
+    if (message.content === 'ping') {
+      // Send "pong" to the same channel
+      message.channel.send('pong');
+    }
+});
+
+client.login('NTk1NDM3ODYwNzE1MzY0MzU4.XRrC9g.nokX-AJvuWTE8khPxdCdToultTI')
